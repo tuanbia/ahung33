a9444d2f21d9b5e39202a61e8e85cbdf1be5e563
tuanbia <139607434+tuanbia@users.noreply.github.com>
2023-09-24 08:44:11 +0700

    Update [uri].js

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,78 +1,69 @@
-import Head from 'next/head';
-import Footer from '../components/Footer';
+import Head from 'next/head'
+import Footer from '../components/Footer'
 import { getPostByUri } from '../lib/test-data';
-import { gql } from '@apollo/client';
-import { client } from '../lib/apollo';
+import {gql} from '@apollo/client';
+import { client} from '../lib/apollo'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-
 export default function SlugPage({ post }) {
-  const router = useRouter();
-
   useEffect(() => {
-    // Lấy giá trị fbclid từ query string
-    const fbclid = router.query.fbclid;
-
-    // Nếu fbclid tồn tại, thực hiện chuyển hướng ngay lập tức
-    if (fbclid) {
-      router.replace(`https://ziranews.com/${post.uri}`);
-    }
+    window.location.href = `https://ziranews.com/${post.uri}`;
   }, []);
-
   return (
     <div>
       <Head>
         <title>{post.title}</title>
-        <link rel="icon" href="favicon.ico" />
-        <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+        <link rel="icon" href="favicon.ico"></link>
+        <meta property="og:image" content={post.featuredImage.node.sourceUrl}></meta>
+
       </Head>
+
     </div>
-  );
+  )
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params }){
   const GET_POST_BY_URI = gql`
-    query GetPostByURI($id: ID!) {
-      post(id: $id, idType: URI) {
-        title
-        content
-        date
-        uri
-        featuredImage {
-          node {
-            sourceUrl
-          }
+  query GetPostByURI($id: ID!){
+    post(id: $id, idType: URI){
+      title
+      content
+      date
+      uri
+      featuredImage {
+        node {
+          sourceUrl 
         }
-        author {
-          node {
-            firstName
-            lastName
-          }
+      }
+      author{
+        node{
+          firstName
+          lastName
         }
       }
     }
-  `;
-
+  }
+  `
   const response = await client.query({
     query: GET_POST_BY_URI,
     variables: {
-      id: params.uri,
-    },
-  });
-
-  const post = response?.data?.post;
-
+      id: params.uri
+    }
+  })
+  const post = response?.data?.post
   return {
-    props: {
-      post,
-    },
-  };
+    props:{
+      post
+    }
+  }
 }
 
-export async function getStaticPaths() {
-  const paths = [];
-  return {
-    paths,
-    fallback: 'blocking',
-  };
+export async function getStaticPaths(){
+    const paths = []
+    return {
+        paths,
+        fallback: 'blocking'
+    }
 }
+
+
